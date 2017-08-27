596010c07af1c467b6792e2b661f820bdfa2c53e
Kylah <kyel07@gmail.com>
2017-08-27 16:49:54 -0700

    Lots of changes - fixed hamburger menu to slide down

--- js/app.js
+++ js/app.js
@@ -1,1 +1,36 @@
-console.log('Hello, your app is working'); 
+// function clickBurger() {
+//     var nav = document.getElementById("nav");
+//     if (nav.style.display === 'none') {
+//         nav.style.display = 'flex';
+//     } else {
+//         nav.style.display ='none';
+//     }
+// }
+
+function clickBurger() {
+    var nav = document.getElementById("nav");
+    if (nav.style.top === '-250px') {
+        nav.style.top = '0px';
+    }
+    else {
+        nav.style.top ='-250px';
+    }
+}
+
+// function closeMenu(){
+//   // $('#nav').fadeOut(200);
+//   var nav = document.getElementById("nav");
+//   if (nav.style.display === 'flex') {
+//       nav.style.display = 'none';
+//   } else {
+//       nav.style.display ='none';
+//   }
+// }
+//
+// $(document.body).click( function(e) {
+//      closeMenu();
+// });
+
+$("#nav").focusout(function(){
+    $(this).style.display ='none';
+});
--- js/main.js
+++ js/main.js
@@ -10252,5 +10252,41 @@ if ( !noGlobal ) {
 return jQuery;
 } );
 
-console.log('Hello, your app is working'); 
+// function clickBurger() {
+//     var nav = document.getElementById("nav");
+//     if (nav.style.display === 'none') {
+//         nav.style.display = 'flex';
+//     } else {
+//         nav.style.display ='none';
+//     }
+// }
+
+function clickBurger() {
+    var nav = document.getElementById("nav");
+    if (nav.style.top === '-250px') {
+        nav.style.top = '0px';
+    }
+    else {
+        nav.style.top ='-250px';
+    }
+}
+
+// function closeMenu(){
+//   // $('#nav').fadeOut(200);
+//   var nav = document.getElementById("nav");
+//   if (nav.style.display === 'flex') {
+//       nav.style.display = 'none';
+//   } else {
+//       nav.style.display ='none';
+//   }
+// }
+//
+// $(document.body).click( function(e) {
+//      closeMenu();
+// });
+
+$("#nav").focusout(function(){
+    $(this).style.display ='none';
+});
+
 //# sourceMappingURL=main.js.map
