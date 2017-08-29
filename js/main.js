de71b262eef79e16a7b181de53a54052b4cb4795
Kylah <kyel07@gmail.com>
2017-08-28 22:05:34 -0700

    Added JS to hide and show content
    Clicking links in the nav menu now hides and shows content

--- js/main.js
+++ js/main.js
@@ -10264,16 +10264,29 @@ function clickBurger() {
 
 
 
+
+
  $(document).ready(function() {
 
-
-
+    //FUNCTION TO HIDE NAV MENU
+        //Click anywhere on the document
+        //If nav menu is showing, hide it
     $(document).on("click", function() {
         if ($('#nav').css('top') == ('0px')) {
                 $('#nav').css('top', '-250px');
             }
-    });
+    }); //End function to hide nav menu
 
-});
+    //FUNCTION TO HIDE AND SHOW CONTENT
+        //Click nav link
+            //the link user clicked on becomes "target"
+            //Show target, hide all sibling divs.
+    $('#nav a').on('click', function(){
+       var target = $(this).attr('rel');
+       $("#"+target).show().siblings("div").hide();
+   }); //End function to hide and show content
+
+
+}); //END READY
 
 //# sourceMappingURL=main.js.map
