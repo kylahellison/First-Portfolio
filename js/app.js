bfd958882c205a4ef893b80ab86310e74c85590e
Kylah <kyel07@gmail.com>
2017-10-05 20:48:42 -0700

    changes to desktop version of portfolio page
    added overlay hover effect on thumbnail images and changed layout

--- js/app.js
+++ js/app.js
@@ -62,6 +62,7 @@ function clickBurger() {
 
     $('#wide-nav a').on('click', function(){
       var targetLI = $(this).parent();
+      
        var target = $(this).attr('rel'); //uses the rel attribute to select the user-selected div by its ID (which matches the rel)
        var header = document.getElementById("head");
        var home = document.getElementById("home");
